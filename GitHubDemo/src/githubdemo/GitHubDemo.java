72463c62e90a1258678fb9f89b51be75a787bd70
se130324 <phuoc-se130324@users.noreply.github.com>
2019-03-25 11:51:45 +0700

    Change line 41

--- GitHubDemo/src/githubdemo/GitHubDemo.java
+++ GitHubDemo/src/githubdemo/GitHubDemo.java
@@ -38,7 +38,7 @@ public class GitHubDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("This is GITHUB test-2");
+        System.out.println("This is GITHUB test-3");
     }
     
 }
